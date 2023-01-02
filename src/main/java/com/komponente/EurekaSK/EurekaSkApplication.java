46018599d2bd76f6568d8844d45c2839ff5bc7b1
Zarko Radenkovic <zradenkovic520rn@raf.rs>
2023-01-02 13:10:32 +0100

    Initial commit

---
+++ src/main/java/com/komponente/EurekaSK/EurekaSkApplication.java
@@ -0,0 +1,15 @@
+package com.komponente.EurekaSK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaSkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaSkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/komponente/EurekaSK/EurekaSkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.komponente.EurekaSK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaSkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
